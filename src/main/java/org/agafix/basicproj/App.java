727774793e9f4c332e2843a815aa99342742cd06
Priyank Singh <001priyank@gmail.com>
2012-04-26 13:04:57 -0800

    first build maven

---
+++ src/main/java/org/agafix/basicproj/App.java
@@ -0,0 +1,13 @@
+package org.agafix.basicproj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
